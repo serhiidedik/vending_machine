d980a4740e7f8a2dde80b13a6638bca936cfefcd
Serhii Dedik <mr.serg.vit@gmail.com>
2026-02-28 19:48:40 +0200

    Run simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+
 RSpec.configure do |config|
+  SimpleCov.start
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
